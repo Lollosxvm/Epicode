cf50ea462c3846ba90a537ac1c20b14e589b977b
Lorenzo <lollosxvm@icloud.com>
2022-11-29 11:42:24 +0100

    first-div

---
+++ Epicode/M3/D3/test.js
@@ -0,0 +1,25 @@
+//how to center a div?
+
+text-align: center;
+
+div
+
+margin: 0 auto;
+
+text-align: center;
+
+margin: 0 auto;
+
+0
+
+auto
+
+width
+
+auto
+
+text-align
+
+text-align: center
+
+
