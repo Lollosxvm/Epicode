1b411013d2753e7fad05c9d52655ce7a31ca7cc4
LorenzoS <112642197+Lollosxvm@users.noreply.github.com>
2022-11-29 14:10:02 +0100

    Add files via upload

---
+++ Epicode/M3/D3/test.js
@@ -0,0 +1,18 @@
+function myFunction1() {
+    let x = document.getElementById("section1");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function myFunction2() {
+    let x = document.getElementById("section2");
+    if (x.style.display === "none") {
+      x.style.display = "flex";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
