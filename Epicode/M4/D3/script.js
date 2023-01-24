554c5af6af242016919b7da7fecfbe9cc37dad78
Lorenzo <lollosxvm@icloud.com>
2023-01-24 16:35:18 +0100

    loadImages()

--- Epicode/M4/D3/script.js
+++ Epicode/M4/D3/script.js
@@ -1,5 +1,5 @@
-
-const api = "https://api.pexels.com/v1/curated"
+// GET API
+const api = "https://api.pexels.com/v1/curated/"
 const token = "EcZN2xYFacKdjKPus0oMZ7sr38DsS6FG4vcdtWDHFwz6S2FW2VCmmJvp"
 
 const options = {
@@ -7,6 +7,18 @@ const options = {
     headers: {
         'Accept': 'application/json',
         'Content-Type': 'application/json',
-        'Authorization': token ,
+        'Authorization': token,
     }
+}
+
+function loadImages() {
+    //prendo l'img
+    return fetch(api, options).then(res => res.json()).then((res) => {
+        res.photos
+        console.log(
+            res.photos,
+            res.photos[0].url
+            
+        )
+    })
 }
