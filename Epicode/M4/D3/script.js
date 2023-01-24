4a5d4bb45a24b17f0985c2a3ee2f50a23d1c03ca
Lorenzo <lollosxvm@icloud.com>
2023-01-24 14:10:15 +0100

    d3 start

---
+++ Epicode/M4/D3/script.js
@@ -0,0 +1,12 @@
+
+const api = "https://api.pexels.com/v1/curated"
+const token = "EcZN2xYFacKdjKPus0oMZ7sr38DsS6FG4vcdtWDHFwz6S2FW2VCmmJvp"
+
+const options = {
+    method: "GET",
+    headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+        'Authorization': token ,
+    }
+}
