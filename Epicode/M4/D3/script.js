7ae8f6b930365ec924e0404244d8bb28e2476bb6
LorenzoS <lorenzosijinardi@libero.it>
2023-01-25 21:04:46 +0100

    modal missing

--- Epicode/M4/D3/script.js
+++ Epicode/M4/D3/script.js
@@ -4,6 +4,8 @@ const apiDue = "https://api.pexels.com/v1/search?query=Tigers"
 const apiTre = "https://api.pexels.com/v1/search?query="
 const token = "EcZN2xYFacKdjKPus0oMZ7sr38DsS6FG4vcdtWDHFwz6S2FW2VCmmJvp"
 
+const modaleCard = document.getElementById("idModale")
+
 const options = {
     method: "GET",
     headers: {
@@ -28,7 +30,7 @@ const createEl = (type, attributes, ...childrens) => {
 
 // creo la card + btn nascondi
 const createCard = (data, container) => {
-    const card = createEl("div", { class: "col-md-4"},
+    const card = createEl("div", { class: "col-md-4" },
         createEl("div", { class: "card mb-4 shadow-sm" },
             createEl("div", { class: "bd-placeholder-img card-img-top", style: `background-image: url(${data.src.medium})` },
             ),
@@ -36,7 +38,7 @@ const createCard = (data, container) => {
                 createEl("p", { class: "card-text" }, `${data.photographer}`),
                 createEl("div", { class: "d-flex justify-content-between align-items-center" },
                     createEl("div", { class: "btn-group" },
-                        createEl("button", { class: "btn btn-sm btn-outline-primary" }, "View"),
+                        createEl("button", { class: "btn btn-sm btn-outline-primary","data-target":"idModale", "data-toggle":"modal" }, "View"),
                         createEl("button", { class: "btn btn-sm btn-outline-secondary", id: data.id }, "Nascondi"),
                     ),
                     createEl("small", { class: "card-text" }, `${data.id}`)
@@ -55,7 +57,7 @@ async function loadSecondImages() {
 
 //Al click di "Load Images" compaiono le card ed il bottone nacondi funziona su ognuna di essa
 const primoBtn = document.querySelector(".btn-primary.my-2");
-
+const Row = document.getElementById("Row");
 primoBtn.addEventListener("click", () => {
     loadImages().then((response) => {
         response.photos.map((photo) => {
@@ -68,42 +70,55 @@ primoBtn.addEventListener("click", () => {
                 card.remove()
             })
         })
+        // const btnModals = document.querySelectorAll(".btn-outline-primary")
+        // btnModals.forEach((btn) =>{
+        //     btn.addEventListener("shown.bs.modal", () => {
+        //         modaleCard.focus()
+        //     })
+        // })
     })
 })
 //Al click di "Load Secondary Images comapiono altre card"
 const secondoBtn = document.querySelector(".btn-secondary.my-2");
+const secondContainer = document.getElementById("secondContainer")
 
 secondoBtn.addEventListener("click", () => {
-    const querySent =
-        loadSecondImages().then((response) => {
-            response.photos.map((photo) => {
-                createCard(photo, Row)
-            })//creo il btn nascondi e relativo funzionamento
-            const btnNascondi = document.querySelectorAll(".btn-outline-secondary")
-            btnNascondi.forEach((btn) => {
-                btn.addEventListener("click", () => {
-                    const card = btn.closest(".col-md-4")
-                    card.remove()
-                })
+    if (Row.children.length > 0) {
+        Row.remove()
+    }
+    loadSecondImages().then((response) => {
+        response.photos.map((photo) => {
+            createCard(photo, secondContainer)
+        })//creo il btn nascondi e relativo funzionamento
+        const btnNascondi = document.querySelectorAll(".btn-outline-secondary")
+        btnNascondi.forEach((btn) => {
+            btn.addEventListener("click", () => {
+                const card = btn.closest(".col-md-4")
+                card.remove()
             })
-        });
-    if (querySent) {
-        // const cancelCard = document.querySelectorAll("col-md-4")
-        // cancelCard.forEach((card) => {
-        //     btn.addEventListener("click", () => {
-        //         const card = btn.closest("col-md-4")
-        //         card.remove()
-    //         })
-    //     })
-    // } else {
-        
-    // }
+        })
+    });
 })
 
-// async function input() {
-//     const data = await fetch(api, options)
-//     return await data.json()
-// }
+const query = document.getElementById("fname").value;
+const invio = document.getElementById("invioQuery");
+const thirdContainer= document.getElementById("thirdContainer");
+async function queryUser(input) {
+    const data = await fetch(`https://api.pexels.com/v1/search?query=${input}`, options)
+    return await data.json()
+}
+invio.addEventListener("click", (e) => {
+    const query = document.getElementById("fname").value;
+    console.log(query)
+    e.preventDefault();
+    queryUser(query).then((response) => {
+        console.log(response)
+        if(thirdContainer.hasChildNodes()){
+            thirdContainer.innerHTML="";
+        }
+        response.photos.map(array => createCard(array,thirdContainer))
+    })
+})
 
 
 
