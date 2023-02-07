3a5d45781b4a7d0d4108c09af38d47e8cd3de92e
LorenzoS <lorenzosijinardi@libero.it>
2023-02-07 20:15:34 +0100

    d1m5

--- Epicode/M4/D8/scripts/back-office.js
+++ Epicode/M4/D8/scripts/back-office.js
@@ -1,11 +1,10 @@
-const submitBtn = document.getElementById("addProductsBtn");
 const api = "https://striveschool-api.herokuapp.com/api/product/";
 const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2M2RhYWNkMjJiMDAxNTAwMTU5YThiNjUiLCJpYXQiOjE2NzUyNzU0NzcsImV4cCI6MTY3NjQ4NTA3N30.g6So5Ksx8gdfO2MVLIwLddxtOjIdw_8fIin71rnO8Ok"
 const url = "https://striveschool-api.herokuapp.com/api/product/"
 const form = document.getElementById("post-form");
 const select = document.getElementById("select-Id");
 const cancelbtn = document.getElementById("cancelProductsBtn");
-
+const submitBtn = document.getElementById("addProductsBtn");
 
 async function postData() {
     if (submitBtn) {
@@ -30,27 +29,6 @@ async function postData() {
 }
 
 
-async function putData() {
-    if (modifyProductsBtn) {
-        const formData = new FormData(form)
-        const data = Array.from(formData.entries()).reduce((acc, [key, value]) => {
-            if (value !== "") {
-                acc[key] = value
-            }
-            return acc
-        }, {})
-        console.log(form)
-        data.price = Number(data.price)
-        await fetch(`https://striveschool-api.herokuapp.com/api/product/${select.value}`, {
-            method: 'PUT',
-            headers: {
-                "Content-type": "application/json",
-                Authorization: "Bearer " + token,
-            },
-            body: JSON.stringify(data)
-        })
-    }
-}
 
 const options = {
     method: "GET",
@@ -65,6 +43,26 @@ submitBtn.addEventListener("click", (e) => {
     e.preventDefault();
     postData()
 })
+async function putData() {
+    if (modifyProductsBtn) {
+        const formData = new FormData(form)
+        const data = Array.from(formData.entries()).reduce((acc, [key, value]) => {
+            if (value !== "") {
+                acc[key] = value
+            }
+            return acc
+        }, {})
+        data.price = Number(data.price)
+        await fetch(`https://striveschool-api.herokuapp.com/api/product/${select.value}`, {
+            method: 'PUT',
+            headers: {
+                "Content-type": "application/json",
+                Authorization: "Bearer " + token,
+            },
+            body: JSON.stringify(data)
+        })
+    }
+}
 
 async function getData() {
     try {
@@ -78,7 +76,7 @@ async function getData() {
 
 async function populateSelect() {
     const product = await getData()
-    console.log(product)
+    //console.log(product)
     product.forEach(element => {
         const opt = document.createElement("option")
         opt.value = element._id
