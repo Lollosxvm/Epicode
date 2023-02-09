cb583de23f9fdd4d20ec90fa565bc3846b529d09
LorenzoS <lorenzosijinardi@libero.it>
2023-02-09 22:08:42 +0100

    react-app

---
+++ Epicode/M5/Esercitazioni/d1/src/components/Navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../css/navbar.css'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <ul>
+            <li><a href="#">Link 1</a></li>
+            <li><a href="#">Link 2</a></li>
+            <li><a href="#">Link 3</a></li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Navbar
